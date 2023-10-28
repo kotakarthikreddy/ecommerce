d7c316b732bc1f7eb07facfd26a45b9342ea99d5
Karthik Kumar Reddy <1234karthikreddy@gmail.com>
2023-10-27 21:43:45 -0500

    Basic Template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
 import logo from './logo.svg';
+import { useEffect, useState } from 'react'
 import './App.css';
+import Header from './Header'
+import Home from './Home'
+import Cart from './Cart'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { db } from './firebase'
 
-function App() {
+function App() { 
+
+  const [ cartItems, setCartItems ] = useState([]);
+
+  useEffect(() => {
+    db.collection('cart-items').onSnapshot((snapshot)=>{
+      let tempCartItems = []
+      snapshot.docs.map((doc)=>{
+          tempCartItems.push({
+              id: doc.id,
+              cartItem: doc.data()
+          })
+      })
+      setCartItems(tempCartItems)
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hi , i am back happy learning kota.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header 
+          cartItems={cartItems} />
+        <Switch>
+          <Route path="/cart">
+            <Cart cartItems={cartItems} />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
